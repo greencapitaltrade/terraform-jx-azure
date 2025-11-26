6a97044466fd5b7bdae1510ea2bc55b402fa8dc5
Amit Kumar <findme@itsamit.com>
2025-11-26 17:37:49 +0530

    fix: update registry configuration to use Nexus instead of ACR

--- requirements.tf
+++ requirements.tf
@@ -1,7 +1,7 @@
 locals {
   jx_requirements_interpolated_content = templatefile("${path.module}/jx-requirements.yml.tpl", {
 
-    registry_name        = lower("${module.registry.registry_name}.azurecr.io")
+    registry_name        = "nexus-nexus-repository-manager.jx.svc.cluster.local:8083"
     domain               = module.dns.domain
     apex_domain          = var.apex_domain
     subdomain            = var.subdomain
